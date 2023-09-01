1be0b9af952b53d26c5c0eaddbc42d677cb55121
treesaretall <himatthewhurst@hotmail.com>
2023-08-31 22:11:21 -0400

    finished navbar and fixed issue with flex display

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
-
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from 'react-icons/ai';
+import { BsPerson } from 'react-icons/bs';
+import { GrProjects } from 'react-icons/gr'
 function Sidenav() {
     const [nav, setNav] = useState(false);
     const handleNav = () => {
@@ -8,7 +9,34 @@ function Sidenav() {
     }
     return (
         <div>
-            <AiOutlineMenu className='absolute top-4 right-4 z-[99]'/>
+            <AiOutlineMenu onClick={handleNav} className='absolute flex top-4 right-4 z-[99] md:hidden' />
+            {
+            nav ? (
+                    <div className='fixed flex w-full h-screen bg-white/90 flex-col justify-center items-center z-20'>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <GrProjects size={20} />
+                            <span className='pl-4'>Work</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <AiOutlineProject size={20} />
+                            <span className='pl-4'>Projects</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <BsPerson size={20} />
+                            <span className='pl-4'>Resume</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <AiOutlineMail size={20} />
+                            <span className='pl-4'>Contact</span>
+                        </a>
+            </div>
+            )
+            : ('')
+            }
         </div>
     )
 }
